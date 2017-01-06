5f2fbdcaf15fe22ab45e2cbb87e8d9ac9cd23793
gvdonovan <gvdonovan@gmail.com>
2017-01-06 11:40:40 -0600

    Added cancellation

--- RamQuest.Workspace.Modules.ShippingServices/ViewModels/AViewModel.cs
+++ RamQuest.Workspace.Modules.ShippingServices/ViewModels/AViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Prism.Commands;
 using Prism.Events;
+using Prism.Interactivity.InteractionRequest;
 using Prism.Regions;
 using RamQuest.Workspace.Infrastructure;
 
@@ -14,11 +15,14 @@ namespace RamQuest.Workspace.Modules.ShippingServices.ViewModels
 
         public DelegateCommand<string> NavigateCommand { get; private set; }
 
+        private readonly InteractionRequest<Confirmation> confirmExitInteractionRequest;
+
         public AViewModel(IRegionManager regionManager, IEventAggregator eventAggregator)
         {
             _regionManager = regionManager;
             _eventAggregator = eventAggregator;
-            NavigateCommand = new DelegateCommand<string>(Navigate);         
+            NavigateCommand = new DelegateCommand<string>(Navigate);
+            confirmExitInteractionRequest = new InteractionRequest<Confirmation>();
         }        
 
         private void Navigate(string obj)
@@ -30,10 +34,26 @@ namespace RamQuest.Workspace.Modules.ShippingServices.ViewModels
         public override void OnNavigatedTo(NavigationContext navigationContext)
         {
             if (navigationContext == null) throw new ArgumentNullException(nameof(navigationContext));
-            if (navigationContext == null) throw new ArgumentNullException(nameof(navigationContext));
             Title = $"{navigationContext.Parameters[MenuFolders.FolderKey]}";
             base.OnNavigatedTo(navigationContext);
             _eventAggregator.GetEvent<ViewActivateEvent>().Publish(Title);
+        }
+
+        public IInteractionRequest ConfirmExitInteractionRequest
+        {
+            get { return this.confirmExitInteractionRequest; }
+        }
+
+        public override void ConfirmNavigationRequest(NavigationContext navigationContext, Action<bool> continuationCallback)
+        {
+            var b = true;
+            if (b)
+            {
+                confirmExitInteractionRequest.Raise(
+                    new Confirmation {Content = "Foo", Title = "Bar"},
+                    c => { continuationCallback(c.Confirmed); });
+            }
+            continuationCallback(true);
         }
     }
 }
